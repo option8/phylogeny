6f19766b21a50d1bf368ddb8c17e3ac23129755e
mac-nerd <option8@option8.com>
2025-10-07 14:01:52 -0400

    speed up. remove scoring.

--- filethread.py
+++ filethread.py
@@ -23,7 +23,7 @@ colors=['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#
 hexDumpArray=[]
 matchesArray=[]
 threadArray=[]
-scoreMatrix=[]
+# scoreMatrix=[]
 bestPath=[]
 filesArray=[]
 
@@ -62,9 +62,9 @@ def update_progress(progress,total):
 def reset():
 	matchesArray=[]
 	threadArray=[]
-	global scoreMatrix
+# 	global scoreMatrix
 	global filesArray
-	scoreMatrix=[]
+# 	scoreMatrix=[]
 
 	global bestPath
 	bestPath=[]
@@ -79,22 +79,23 @@ def reset():
 	for fileNum in range(0,len(filesArray)):
 	
 		print(filesArray[fileNum])
-	
+
+# skip this and open as text if file is text?	
 		binary=open(filesArray[fileNum],"rb").read()
 		
 		hexDumpArray.append(hexdump.dump(binary).replace("00 00 ", "") )	# remove 00 padding in sparse data
 		
 		pVScale = VSIZE/(len(hexDumpArray[fileNum])/3) 	# fit the largest file vertically, but scale all files the same amount. 
-															# Can't do this with binary size since some are 00 padded or "sparse"
+														# Can't do this with original binary size since some are 00 padded or "sparse"
 		if pVScale < VScale:
 			VScale = pVScale
 			
 		# set up score matrix for sorting
-		scoreMatrix.append([])
+# 		scoreMatrix.append([])
 	
-	while len(scoreMatrix[0]) < len(scoreMatrix): # make a square matrix
-		for colNum in range(0,len(scoreMatrix)):
-			scoreMatrix[colNum].append([])
+# 	while len(scoreMatrix[0]) < len(scoreMatrix): # make a square matrix
+# 		for colNum in range(0,len(scoreMatrix)):
+# 			scoreMatrix[colNum].append([])
 #	print(scoreMatrix)
 
 
@@ -108,197 +109,200 @@ def reset():
 
 def matchBlocks( matchType ):	
 
-	global bestPath
-	global scoreMatrix
+# 	global bestPath
+# 	global scoreMatrix
 
 #	print(scoreMatrix)
 
-	for originFile in range(0, len(hexDumpArray)):
+#	for originFile in range(0, len(hexDumpArray)):
+	for originFile in range(0, len(hexDumpArray)-1):
 		print(f" Completed {originFile} of {len(hexDumpArray)}") 
 #		print(scoreMatrix[0])
-		scoreMatrix[originFile][originFile]=0
+# 		scoreMatrix[originFile][originFile]=0
 		
-		for targetFile in range(originFile+1, len(hexDumpArray)):
-			matchCount=0
-			matchSum=0
-			diffSum=0
-			
-			lastResult=""
-			originByte=0
-			originLength = math.floor( (len(hexDumpArray[originFile])-3 ) / 3)
-			
-			print(f" {targetFile}/{len(hexDumpArray)}")
-			while originByte < originLength :
-		
-				update_progress(originByte, originLength)
-		
-				for byteLength in range(minRange,10240):
-			
-					if int(originByte + byteLength) < int(originLength) : # don't try to read past the end. oof.
-					
-						charLength = int(3 * byteLength) # searching for hexdump strings, so 3 chars per byte
-						originChars = int(3 * originByte) # searching for hexdump strings, so 3 chars per byte
-								
-						byteBlock = hexDumpArray[originFile][originChars:originChars+charLength] # block of bytes to search for
+#		for targetFile in range(originFile+1, len(hexDumpArray)):
 
-					# do the actual search	- just for the first instance
-						wordIndex = hexDumpArray[targetFile].find(byteBlock)
-								
-						if wordIndex != -1:
-							byteIndex = int(wordIndex/3)		
-							lastResult= f"{originFile}:{targetFile}\t{byteLength}\t{originByte}\t{byteIndex}"
+		targetFile = originFile+1
+		matchCount=0
+		matchSum=0
+		diffSum=0
+		
+		lastResult=""
+		originByte=0
+		originLength = math.floor( (len(hexDumpArray[originFile])-3 ) / 3)
+		
+		print(f" {targetFile}/{len(hexDumpArray)}")
+		while originByte < originLength :
+	
+			update_progress(originByte, originLength)
+	
+			for byteLength in range(minRange,10240):
+		
+				if int(originByte + byteLength) < int(originLength) : # don't try to read past the end. oof.
+				
+					charLength = int(3 * byteLength) # searching for hexdump strings, so 3 chars per byte
+					originChars = int(3 * originByte) # searching for hexdump strings, so 3 chars per byte
 							
-						else:
-									
-							if len(lastResult) > 0: #success!
+					byteBlock = hexDumpArray[originFile][originChars:originChars+charLength] # block of bytes to search for
 
-								currentMatch=[]
-								currentMatch.append(byteLength-1)
-
-								for i in range(0,originFile):
-									currentMatch.append(None)
-
-								currentMatch.append(originByte)
-
-								for i in range(0, (targetFile - originFile) - 1):
-									currentMatch.append(None)
+				# do the actual search	- just for the first instance
+					wordIndex = hexDumpArray[targetFile].find(byteBlock)
+							
+					if wordIndex != -1:
+						byteIndex = int(wordIndex/3)		
+						lastResult= f"{originFile}:{targetFile}\t{byteLength}\t{originByte}\t{byteIndex}"
+						
+					else:
 								
-								currentMatch.append(byteIndex)
+						if len(lastResult) > 0: #success!
 
-								for i in range(targetFile, len(hexDumpArray)-1):
-									currentMatch.append(None)
-								
+							currentMatch=[]
+							currentMatch.append(byteLength-1)
+
+							for i in range(0,originFile):
+								currentMatch.append(None)
+
+							currentMatch.append(originByte)
+
+							for i in range(0, (targetFile - originFile) - 1):
+								currentMatch.append(None)
+							
+							currentMatch.append(byteIndex)
+
+							for i in range(targetFile, len(hexDumpArray)-1):
+								currentMatch.append(None)
+							
 #								print(currentMatch)
 
-								matchesArray.append(currentMatch)
+							matchesArray.append(currentMatch)
 
-								indexDiff=abs(byteIndex - originByte)
-								if indexDiff > byteLength:
-									indexDiff = byteLength/2
-								elif indexDiff == 0:
-									indexDiff = byteLength * 2
-								else:
-									indexDiff = byteLength
-								
-								matchCount+=1
-								matchSum+=indexDiff
-								lastResult="" # reset for next go-round
-							break
-					else: 
+							indexDiff=abs(byteIndex - originByte)
+							if indexDiff > byteLength:
+								indexDiff = byteLength/2
+							elif indexDiff == 0:
+								indexDiff = byteLength * 2
+							else:
+								indexDiff = byteLength
+							
+							matchCount+=1
+							matchSum+=indexDiff
+							lastResult="" # reset for next go-round
 						break
-					
-					
-				if len(lastResult) > 0:	### matched on end of file/bytesize loop
-					
-					byteIndex = int(wordIndex/3)
+				else: 
+					break
+				
+				
+			if len(lastResult) > 0:	### matched on end of file/bytesize loop
+				
+				byteIndex = int(wordIndex/3)
 
-					currentMatch=[]
-					currentMatch.append(byteLength-1)
-	
-					for i in range(0,originFile):
-						currentMatch.append(None)
-	
-					currentMatch.append(originByte)
-	
-					for i in range(0, (targetFile - originFile) - 1):
-						currentMatch.append(None)
-					
-					currentMatch.append(byteIndex)
+				currentMatch=[]
+				currentMatch.append(byteLength-1)
 
-					for i in range(targetFile, len(hexDumpArray)-1):
-						currentMatch.append(None)
+				for i in range(0,originFile):
+					currentMatch.append(None)
+
+				currentMatch.append(originByte)
+
+				for i in range(0, (targetFile - originFile) - 1):
+					currentMatch.append(None)
+				
+				currentMatch.append(byteIndex)
+
+				for i in range(targetFile, len(hexDumpArray)-1):
+					currentMatch.append(None)
 
 #					print(currentMatch)
-					matchesArray.append(currentMatch)
-					
-					
+				matchesArray.append(currentMatch)
+				
+				
 ### TBD: adjust scoring for byte offset differences					
-					indexDiff=abs(byteIndex - originByte)
-					if indexDiff > byteLength:
-						indexDiff = byteLength/2
-					elif indexDiff == 0:
-						indexDiff = byteLength * 2
-					else:
-						indexDiff = byteLength
-					
-					matchCount+=1
-					matchSum+=indexDiff
-					
-					lastResult="" # reset for next go-round
-					
-				originByte = originByte+(byteLength-2) # skip ahead past matched area, but overlap last 2 bytes 
-			
-			try:
-				rowScore = math.sqrt( matchCount * matchSum )	## in case it's negative or 0.
-			except:
-				rowScore = 0
-			finally:
-				#scoreRow.append(rowScore)
-				scoreMatrix[originFile][targetFile]=rowScore
-				scoreMatrix[targetFile][originFile]=rowScore
-	
+				indexDiff=abs(byteIndex - originByte)
+				if indexDiff > byteLength:
+					indexDiff = byteLength/2
+				elif indexDiff == 0:
+					indexDiff = byteLength * 2
+				else:
+					indexDiff = byteLength
+				
+				matchCount+=1
+				matchSum+=indexDiff
+				
+				lastResult="" # reset for next go-round
+				
+			originByte = originByte+(byteLength-2) # skip ahead past matched area, but overlap last 2 bytes 
+		
+# 		try:
+# 			rowScore = math.sqrt( matchCount * matchSum )	## in case it's negative or 0.
+# 		except:
+# 			rowScore = 0
+# 		finally:
+# 			#scoreRow.append(rowScore)
+# 			scoreMatrix[originFile][targetFile]=rowScore
+# 			scoreMatrix[targetFile][originFile]=rowScore
+
 	
 #		scoreMatrix.append(scoreRow)
 
 
 #	print(scoreMatrix)
 
-	with open('filethread.csv', 'w', newline='') as file:
-		writer = csv.writer(file)
-		writer.writerows(scoreMatrix)
-	pathsArray=[]
-	pathLengthArray=[]
+# 	with open('filethread.csv', 'w', newline='') as file:
+# 		writer = csv.writer(file)
+# 		writer.writerows(scoreMatrix)
+# 	pathsArray=[]
+# 	pathLengthArray=[]
 
 ### what follows is all made redundant by pbzCompare.sh
-	if matchType=="full":	
-		for startNode in range(0,len(scoreMatrix)):
-		
-			# brute force nearest neighbors.
-			# checklist = 0 - len(scoreMatrix)
-			nodeList = [x for x in range(0, len(scoreMatrix))]
-			# start with node 0
-		#	startNode = 0
-			pathLength = 0 
-			currentNode = startNode
-			currentPath = []
-			
-			while len(nodeList) > 0:
-	#			print("NODES", nodeList)
-				
-	#			print(f"ROW {currentNode}", scoreMatrix[currentNode])
-				
-				# remove that from the checklist
-				if currentNode in nodeList:
-					nodeList.remove(currentNode)
-					currentPath.append(currentNode)
-				else:
-					break
-				
-				# find nearest node in scoreMatrix
-				# nextNode = scoreMatrix[currentNode].index(min(scoreMatrix[currentNode]))
-				
-				maxScore=0
-
-		
-				for candidate in nodeList:
-	#				print(scoreMatrix[currentNode][candidate])
-		
-					if scoreMatrix[currentNode][candidate] > maxScore:
-						maxScore = scoreMatrix[currentNode][candidate]
-						nextNode = candidate
-				
-	#			print("NEXT", nextNode, minScore)
-				pathLength += maxScore
-	
-				currentNode = nextNode
-	
-			print(f"Path {startNode} {currentPath} length:", pathLength) ## all out of nodes?
-			pathsArray.append(currentPath)
-			pathLengthArray.append(pathLength)
-
-	
-		bestPath=pathsArray[pathLengthArray.index(max(pathLengthArray))]
-		print(f"Best path: {bestPath}")
+# 	if matchType=="full":	
+# 		for startNode in range(0,len(scoreMatrix)):
+# 		
+# 			# brute force nearest neighbors.
+# 			# checklist = 0 - len(scoreMatrix)
+# 			nodeList = [x for x in range(0, len(scoreMatrix))]
+# 			# start with node 0
+# 		#	startNode = 0
+# 			pathLength = 0 
+# 			currentNode = startNode
+# 			currentPath = []
+# 			
+# 			while len(nodeList) > 0:
+# 	#			print("NODES", nodeList)
+# 				
+# 	#			print(f"ROW {currentNode}", scoreMatrix[currentNode])
+# 				
+# 				# remove that from the checklist
+# 				if currentNode in nodeList:
+# 					nodeList.remove(currentNode)
+# 					currentPath.append(currentNode)
+# 				else:
+# 					break
+# 				
+# 				# find nearest node in scoreMatrix
+# 				# nextNode = scoreMatrix[currentNode].index(min(scoreMatrix[currentNode]))
+# 				
+# 				maxScore=0
+# 
+# 		
+# 				for candidate in nodeList:
+# 	#				print(scoreMatrix[currentNode][candidate])
+# 		
+# 					if scoreMatrix[currentNode][candidate] > maxScore:
+# 						maxScore = scoreMatrix[currentNode][candidate]
+# 						nextNode = candidate
+# 				
+# 	#			print("NEXT", nextNode, minScore)
+# 				pathLength += maxScore
+# 	
+# 				currentNode = nextNode
+# 	
+# 			print(f"Path {startNode} {currentPath} length:", pathLength) ## all out of nodes?
+# 			pathsArray.append(currentPath)
+# 			pathLengthArray.append(pathLength)
+# 
+# 	
+# 		bestPath=pathsArray[pathLengthArray.index(max(pathLengthArray))]
+# 		print(f"Best path: {bestPath}")
 
 # for each match found from file n to n+1, look for it in files n+2...
 # blocklength, file0loc, file1loc, ...filenloc
@@ -322,17 +326,17 @@ def doThreads( matchType ):
 				byteBlock = hexDumpArray[originFile][originChars:originChars + charLength] # block of bytes to search for
 				break
 ### redundant, again, but also overkill and makes the display too messy.
-		if matchType=="full":
-			for targetFile in range(0,len(hexDumpArray)):
-				if blockMatch[targetFile+1]==None:
-					wordIndex=hexDumpArray[targetFile].find(byteBlock)
-					if wordIndex != -1:
-						byteIndex = int(wordIndex/3)
-	#					print("found new match in ",targetFile+1)
-					else:
-						byteIndex = -1
-					
-					blockMatch[targetFile+1]= byteIndex
+# 		if matchType=="full":
+# 			for targetFile in range(0,len(hexDumpArray)):
+# 				if blockMatch[targetFile+1]==None:
+# 					wordIndex=hexDumpArray[targetFile].find(byteBlock)
+# 					if wordIndex != -1:
+# 						byteIndex = int(wordIndex/3)
+# 	#					print("found new match in ",targetFile+1)
+# 					else:
+# 						byteIndex = -1
+# 					
+# 					blockMatch[targetFile+1]= byteIndex
 			
 			
 def drawThreads() :
