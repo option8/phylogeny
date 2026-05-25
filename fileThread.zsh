dad5cb579ef55e617f0402a5b7fe96e603125202
mac-nerd <option8@option8.com>
2026-05-25 13:29:15 -0400

    visual update, command line options

--- fileThread.zsh
+++ fileThread.zsh
@@ -7,6 +7,37 @@
 # Based on concepts explained pretty well in this video:
 #	https://youtu.be/aLaYgzmRPa8?si=3t9V86oC-DqTrFyT
 
+doHelp() {
+
+cat << EOF
+Command Line options:	
+ -b		parse as binary (default)
+ -t		force parsing as text
+ -o [filename]	generates filename.svg (default "File-Ogeny.svg")
+ -r		reorders (default) and reverses result
+ -s		skips the pathfinding step, parses files in order they are given (overrides -r)
+ -m [integer]	minimum size (characters/bytes) of matching regions (default 8)
+ -h		print this help
+ -p		just print out phylogeny distance matrix, suitable for analysis here:
+		https://www.trex.uqam.ca/index.php?action=trex&menuD=1&method=2
+	
+EOF
+	exit 0
+
+
+
+}
+
+
+if [[ "$#" -eq 0 || "$0" == "-h" ]]
+then
+
+	doHelp
+	
+fi
+
+
+
 zmodload zsh/mathfunc
 
 binDir=$(dirname $0)
@@ -17,18 +48,49 @@ arrayIndex=1
 
 TEMPDIR=$(mktemp -d -t fileThread)
 
-# command line switch for binary vs text parsing
-zparseopts -D -E -F -K -- b+=doBinary t+=doText o:=outputFile s+=skipReorder
-### -b				parse as binary (default)
-### -t				parse as text
-### -o filename		generates filename.PNG and filename.SVG 
+zparseopts -D -E -F -K -- b+=doBinary t+=doText o:=outputFile s+=skipReorder r+=reverseReorder m:=minBytes p+=doPhyloTree h+=showHelp
+### -b			parse as binary (default)
+### -t			force parsing as text
+### -o [filename]	generates filename.svg (default "File-Ogeny.svg")
+### -r			reorders and reverses result
+### -s			skips the pathfinding step, parses files in order they are given (overrides -r)
+### -m [integer]	minimum size of matching regions (default 8)
+### -h			print this help 
+### -p			just print out phylogeny distance matrix
+
+### TODO: rename inputs with MD5 hash, sort out exact dupes
+
+if [[ ! $(which pbzip2) ]]
+then
+	echo "This tool requires pbzip2"
+	echo "Install it with\n\tbrew install pbzip2"
+	exit 0
+fi
+
 
 outputFile=$outputFile[-1] # either provided on the command line, or default path
+minBytes=$minBytes[-1]
 
 if [[ -z $outputFile ]] 
 then
-	outputFile="filethread"
+	outputFile="File-Ogeny.svg"
 fi
+
+if [[ ${${outputFile##*.}:l} != "svg" ]]
+then
+	outputFile="$outputFile.svg"
+fi
+
+if [[ -z $minBytes ]] 
+then
+	minBytes=8
+fi
+
+if [[ ${#showHelp} -gt 0 ]]
+then
+	doHelp
+fi
+
 
 if [[ ${#doText} -gt 0 ]]
 then
@@ -45,7 +107,7 @@ then
 
 	done
 else # default
-	threadMode="bin"	
+	threadMode="bin"
 fi
 
 
@@ -53,17 +115,25 @@ fi
 
 if [[ ${#skipReorder} -gt 0 ]]
 then
-	threadMode="skip"
+	reorder="skip"
 	pathsArray=({0..$(($# * $#))})
+else 	
+	if [[ ${#reverseReorder} -gt 0 ]]
+	then
+		reorder="reverse"
+	else
+		reorder="true"
+	fi
 	
-else 
 	echo "$# Files to consider."
 	# with each file in the command line to test
+	TreeRows=()
+
 	for fileNum in $(seq 1 $#)
 	do
-		
+
 		TESTFile=${argv[$fileNum]}
-	
+		TreeRows[fileNum]=""
 		# compress it into a temp file with pbzip2
 		cat "$TESTFile" | pbzip2 -c > "$TEMPDIR/Temp1"
 		
@@ -94,12 +164,12 @@ else
 	
 				# the similarity score is the ratio of the size of the combined compressed files and the sum of the separately compressed files
 				comboScore=$(( (1.0000 * $(stat -f %z "$TEMPDIR/TempZ")/CompCat) ))
-							
+						
 			fi
 			
 			# write that down.
 			pathsArray[$arrayIndex]="$comboScore"
-			
+			TreeRows[fileNum]+="\t$comboScore"
 			# show progress.
 			printf ". "
 			
@@ -113,9 +183,26 @@ else
 
 fi
 
+if [[ ${#doPhyloTree} -gt 0 ]] # just do the distance tree, no threading.
+then
 
+	echo "Paste distance matrix into https://www.trex.uqam.ca/index.php?action=trex&menuD=1&method=2"
+	echo " === "
+	
+	echo $#
+	for fileNum in $(seq 1 $#)
+		do
+			normalizedFile=$(basename "${argv[$fileNum]}" | tr -c "[:alnum:]" ".")
+			echo "$normalizedFile\t$TreeRows[fileNum]"
+	done
 
-# run the magic python "shortest path" script
-python3 $binDir/findPath.py $threadMode $outputFile ${argv[@]} $pathsArray
+else
+	
+	echo "Finding threads. Smallest region: $minBytes Bytes."
+	
+	# run the magic python "shortest path" script
+	python3 $binDir/findPath.py $reorder $threadMode $outputFile $minBytes ${argv[@]} $pathsArray
+	
+	open -a Google\ Chrome ${outputFile}
 
-open "${outputFile}.png"
+fi
